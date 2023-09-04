9262e0a8f5d15415f71fb87180ddb491693b1c9b
Vikram Revanna <vikram.k@mastek.com>
2023-09-04 13:21:05 +0530

    removed file

--- InstallChrome.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get install chromium-chromedriver
-cd /usr/lib/chromium-browser/
-ls
-export PATH=$PATH:/usr/lib/chromium-browser/
-sudo mv chromedriver /usr/bin/chromedriver
-sudo chown root:root /usr/bin/chromedriver
-sudo chmod +x /usr/bin/chromedriver
