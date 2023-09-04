c46f58ab5b16e8550cddb4dfba91742e66dc9fa7
vikram-epi <125977586+vikram-epi@users.noreply.github.com>
2023-09-04 12:28:44 +0530

    Update InstallChrome.sh

--- InstallChrome.sh
+++ InstallChrome.sh
@@ -3,3 +3,6 @@ sudo apt-get install chromium-chromedriver
 cd /usr/lib/chromium-browser/
 ls
 export PATH=$PATH:/usr/lib/chromium-browser/
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chown root:root /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
